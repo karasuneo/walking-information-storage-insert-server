f7699507b56cfdc49ffdc33e4e2889719406ec6f
karasuneo <yusei4520030401@gmail.com>
2024-12-21 10:05:57 +0900

    add: 固定値の追加

--- server/config/const/__init__.py
+++ server/config/const/__init__.py
@@ -2,9 +2,11 @@ from .bucket import (
                      ACCELEROMETER_BUCKET_NAME,
                      ATMOSPHERIC_PRESSURE_BUCKET_NAME,
                      BUCKET_NAME,
+                     COORDINATE_BUCKET_NAME,
                      FLOOR_BUCKET_NAME,
                      FLOOR_INFORMATION_BUCKET_NAME,
                      FLOOR_MAP_BUCKET_NAME,
+                     FP_MODEL_BUCKET_NAME,
                      GPS_BUCKET_NAME,
                      GYROSCOPE_BUCKET_NAME,
                      GYROSCOPE_FILE_BUCKET_NAME,
@@ -12,6 +14,15 @@ from .bucket import (
                      PEDESTRIAN_BUCKET_NAME,
                      RATIO_WAVE_BUCKET_NAME,
                      WALKING_INFORMATION_BUCKET_NAME,
+)
+from .extension import (
+                     ACCELEROMETER_EXTENSION,
+                     ATMOSPHERIC_PRESSURE_EXTENSION,
+                     FLOOR_MAP_EXTENSION,
+                     FP_MODEL_EXTENSION,
+                     GPS_EXTENSION,
+                     GYROSCOPE_EXTENSION,
+                     RATIO_WAVE_EXTENSION,
 )
 
 __all__ = [
@@ -28,4 +39,13 @@ __all__ = [
     "PEDESTRIAN_BUCKET_NAME",
     "RATIO_WAVE_BUCKET_NAME",
     "WALKING_INFORMATION_BUCKET_NAME",
+    "COORDINATE_BUCKET_NAME",
+    "FP_MODEL_BUCKET_NAME",
+    "FP_MODEL_EXTENSION",
+    "ACCELEROMETER_EXTENSION",
+    "ATMOSPHERIC_PRESSURE_EXTENSION",
+    "FLOOR_MAP_EXTENSION",
+    "GPS_EXTENSION",
+    "GYROSCOPE_EXTENSION",
+    "RATIO_WAVE_EXTENSION",
 ]
